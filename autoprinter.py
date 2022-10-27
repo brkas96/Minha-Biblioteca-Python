a9f99c00f50d701ec1df9257277117d173505e8f
BrunoBenvenutti <69966982+BrunoBenvenutti@users.noreply.github.com>
2022-10-27 13:09:13 -0300

    Atualização do autoprinter

--- autoprinter.py
+++ autoprinter.py
@@ -1,52 +1,48 @@
-#BOOK
+# Printe livros :D
 import os
 import time
 import PySimpleGUI as sg
 import pyautogui
 
-def main():
+def printL():
     pyautogui.alert('Coloque na tela a ser printada e aperte OK :D')
     n = int(0)
     destino = (fullpath)
 
-    while True:
+    while n < paginas:
         c = str(destino + '\pg' + str(n) + '.png')
         img = pyautogui.screenshot()
-        time.sleep(5)
+        time.sleep(1)
         img.save(c)
         pyautogui.press('right')
         n = int(n)
         n = n + 1
 
 layout = [
-    [sg.Text('Digite o Nome da pasta onde os prints ficarão: ')],
+    [sg.Text('Digite o Nome do Livro: ')],
     [sg.Input('', enable_events=True, key='-INPUT-')],
+    [sg.Text('Digite a Quantidade de paginas: ')],
+    [sg.Input('', enable_events=True, key='-NUM-')],
     [sg.Button('Ok', key='-OK-'), sg.Button('Exit')]
 ]
-janela = sg.Window('AutoPrinter(Feito por Bruno Benvenutti Castro)', layout)
+janela = sg.Window('AutoPrinter by BBC :D', layout)
 
 while True:
     event, values = janela.read()
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
     if event == '-OK-':
-        pasta = ''
-        pasta = values['-INPUT-']
-        fullpath = 'C:/AutoPrinter/' + str(pasta)
+        livro = ''
+        livro = values['-INPUT-']
+        paginas = values['-NUM-']
+        paginas = int(paginas)
+        fullpath = 'C:/Livros/' + str(livro)
         janela.close()
         try:
             os.makedirs(fullpath)
-            main()
+            printL()
         except:
-            pyautogui.alert(':0  OU o diretório já exite OU o programa foi encerrado OU houve algum '
-                            'erro que eu não faço idéia de qual seja :P . Vc pode me mandar um email'
-                            ' reclamando, mas vai ter que descobrir qual é primeiro :D')
-
+            pyautogui.alert('Houve um erro. Tente excluir o diretorio C:/Livros. Não use nomes muito longos para nomear a pasta do'
+                            'livro e não use caracteres especiais.')
 janela.close()
-
-#origem = os.path.dirname(os.path.realpath(__file__)) #pega o caminho atual
-#caminho = str(Path.home()) # pega o diretório do usuário ex: C:/User/NomeDoUsuario
-
-
-
 
